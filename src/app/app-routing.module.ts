dd18c87a363eb646236b2750c3bfdb7dea4bd912
David Ma <dmlh02@gmail.com>
2023-06-02 17:37:42 +0100

    Some files updated.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 
 const routes: Routes = [
-  { path: 'intro', component: MainComponent },
+  { path: '', component: MainComponent },
   // { path: 'intro', component: MainComponent },
   { path: '', redirectTo: '/intro', pathMatch: 'full' },
   { path: '**', component: MainComponent }
