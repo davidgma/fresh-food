a1adb4b3510897253098ea031b307adbef87c332
David Ma <dmlh02@gmail.com>
2023-06-02 17:44:00 +0100

    Some files updated.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { MainComponent } from './pages/main/main.component';
 const routes: Routes = [
   { path: '', component: MainComponent },
   // { path: 'intro', component: MainComponent },
-  { path: '', redirectTo: '/intro', pathMatch: 'full' },
+  // { path: '', redirectTo: '/intro', pathMatch: 'full' },
   { path: '**', component: MainComponent }
 ];
 
